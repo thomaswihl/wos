b728523a47fe509c191325f1e78f31c2693f3024
Thomas Wihl <thomas@speedi.local>
2016-02-13 21:15:49 +0100

    SDRAM working

--- memorycontroller.cpp
+++ memorycontroller.cpp
@@ -5,3 +5,71 @@ MemoryController::MemoryController(System::BaseAddress base) :
 {
     static_assert(sizeof(FMC) == 0x15c, "Struct has wrong size, compiler problem.");
 }
+
+void MemoryController::sdRamConfig(SdRam *config1, SdRam *config2, SdRamClock clock, bool singleReadIsBurst)
+{
+    __SDCMR mode;
+    mode.value = 0;
+    if (config1 != nullptr)
+    {
+        mBase->SDCR1.value = config1->cr();
+        mBase->SDTR1.value = config1->tr();
+        mode.bits.CTB1 = 1;
+    }
+    if (config2 != nullptr)
+    {
+        mBase->SDCR2.value = config2->cr();
+        mBase->SDTR2.value = config2->tr();
+        mode.bits.CTB2 = 1;
+    }
+    mBase->SDCR1.bits.SDCLK = static_cast<uint32_t>(clock);
+    mBase->SDCR1.bits.RBURST = singleReadIsBurst;
+    mode.bits.MODE = 1;
+    mBase->SDCMR = mode.value;
+    while (mBase->SDSR.BUSY) { }
+
+    System::instance()->usleep(100);
+
+    mode.bits.MODE = 2;
+    mBase->SDCMR = mode.value;
+    while (mBase->SDSR.BUSY) { }
+
+    mode.bits.MODE = 3;
+    mode.bits.NRFS = 8;
+    mBase->SDCMR = mode.value;
+    while (mBase->SDSR.BUSY) { }
+    mode.bits.NRFS = 0;
+
+    mode.bits.MODE = 4;
+    // Burst Length = 8, CAS Latency = 2
+    mode.bits.MRD = 0x23;
+    mBase->SDCMR = mode.value;
+    while (mBase->SDSR.BUSY) { }
+
+    mBase->SDRTR.COUNT = 0x603;
+
+
+}
+
+MemoryController::SdRam::SdRam(MemoryController::SdRam::Bank bank, MemoryController::SdRam::CasLatency latency, MemoryController::SdRam::DataBus width, MemoryController::SdRam::RowAddress rows, MemoryController::SdRam::ColumnAddress cols, MemoryController::SdRam::BankCount bankCount)
+{
+    mSdcr.bits.MWID = static_cast<uint32_t>(width);
+    mSdcr.bits.NB = static_cast<uint32_t>(bankCount);
+    mSdcr.bits.CAS = static_cast<uint32_t>(latency);
+    mSdcr.bits.NR = static_cast<uint32_t>(rows);
+    mSdcr.bits.NC = static_cast<uint32_t>(cols);
+    mSdcr.bits.WP = 0;
+    mSdcr.bits.RBURST = 1;
+    mBank = bank;
+}
+
+void MemoryController::SdRam::setTiming(int rowToColumn, int rowPrecharge, int recovery, int rowCycle, int selfRefresh, int exitSelfRefresh, int loadModeRegister)
+{
+    mSdtr.bits.TRCD = rowToColumn - 1;
+    mSdtr.bits.TRP = rowPrecharge - 1;
+    mSdtr.bits.TWR = recovery - 1;
+    mSdtr.bits.TRC = rowCycle - 1;
+    mSdtr.bits.TRAS = selfRefresh - 1;
+    mSdtr.bits.TXSR = exitSelfRefresh - 1;
+    mSdtr.bits.TMRD = loadModeRegister - 1;
+}
--- memorycontroller.h
+++ memorycontroller.h
@@ -7,7 +7,13 @@
 class MemoryController
 {
 public:
+    class SdRam;
+    enum class SdRamClock { Disabled, OneHalf = 2, OneThird };
+
     MemoryController(System::BaseAddress base);
+
+    void sdRamConfig(SdRam* config1, SdRam* config2, SdRamClock clock, bool singleReadIsBurst = true);
+
 
 private:
     struct __BCR
@@ -67,7 +73,19 @@ private:
         uint32_t TRCD : 4;
         uint32_t __RESERVED0 : 4;
     };
-
+    union __SDCMR
+    {
+        struct
+        {
+        uint32_t MODE : 3;
+        uint32_t CTB2 : 1;
+        uint32_t CTB1 : 1;
+        uint32_t NRFS : 4;
+        uint32_t MRD : 13;
+        uint32_t __RESERVED0 : 10;
+        }   bits;
+        uint32_t value;
+    };
     struct FMC
     {
         __BCR BCR1; // 0
@@ -94,19 +112,27 @@ private:
         uint32_t __RESERVED5;
         uint32_t BWTR4;
         uint32_t __RESERVED6[8];
-        __SDCR SDCR1;  // 0x140
-        __SDCR SDCR2;
-        __SDTR SDTR1;
-        __SDTR SDTR2;
-        struct __SDCMR
+        union
         {
-            uint32_t MODE : 3;
-            uint32_t CTB2 : 1;
-            uint32_t CTB1 : 1;
-            uint32_t NRFS : 4;
-            uint32_t MRD : 13;
-            uint32_t __RESERVED0 : 10;
-        }   SDCMR; // 0x150
+            __SDCR bits;  // 0x140
+            uint32_t value;
+        }   SDCR1;
+        union
+        {
+            __SDCR bits;  // 0x140
+            uint32_t value;
+        }   SDCR2;
+        union
+        {
+            __SDTR bits;
+            uint32_t value;
+        }   SDTR1;
+        union
+        {
+            __SDTR bits;
+            uint32_t value;
+        }   SDTR2;
+        uint32_t SDCMR; // 0x150
         struct __SDRTR
         {
             uint32_t CRE : 1;
@@ -125,6 +151,41 @@ private:
     };
 
     volatile FMC* mBase;
+
+public:
+    class SdRam
+    {
+    public:
+        enum class Bank { Bank1, Bank2 };
+        enum class BankCount { Two, Four };
+        enum class CasLatency { Cycle1 = 1, Cycle2, Cycle3 };
+        enum class DataBus { Width8, Width16, Width32 };
+        enum class RowAddress { Bits11, Bits12, Bits13 };
+        enum class ColumnAddress { Bits8, Bits9, Bits10, Bits11 };
+
+        SdRam(Bank bank, CasLatency latency, DataBus width, RowAddress rows, ColumnAddress cols, BankCount bankCount);
+
+        void setReadOnly(bool readOnly) { mSdcr.bits.WP = readOnly; }
+        void setTiming(int rowToColumn, int rowPrecharge, int recovery, int rowCycle, int selfRefresh, int exitSelfRefresh, int loadModeRegister);
+
+        uint32_t cr() const { return mSdcr.value; }
+        uint32_t tr() const { return mSdtr.value; }
+
+    private:
+        union
+        {
+            MemoryController::__SDCR bits;
+            uint32_t value;
+        }   mSdcr;
+        union
+        {
+            MemoryController::__SDTR bits;
+            uint32_t value;
+        }   mSdtr;
+        Bank mBank;
+    };
+
+
 };
 
 #endif // MEMORYCONTROLLER_H
